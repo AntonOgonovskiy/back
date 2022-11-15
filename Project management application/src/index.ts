8da057c1361e829aefb5aac4626f142bfff81b80
Anton <antonogonovskiy@gmail.com>
2022-11-15 19:46:43 +0300

    feat: add login & password

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,23 +1,22 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}');
+    await mongoose.connect(
+      "mongodb+srv://Anton:D9k5NSSEjL39WuEU@cluster0.sjvjpdj.mongodb.net/kanban"
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
